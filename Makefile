99c7121fbf068a0ec9942a72ef315c9911a27429
olegfomenko <oleg.fomenko2002@gmail.com>
2025-10-31 12:45:09 +0200

    adding separate test method for file

--- Makefile
+++ Makefile
@@ -2,4 +2,9 @@ debug:
 	bash scripts/debug.sh
 
 test: 
+- all:
 	bash scripts/test.sh
+- file:
+	bash scripts/test-file.sh $(name)
+
+
---
+++ scripts/test-file.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rm -rf ./tmp
+mkdir ./tmp
+
+rm -rf ./target
+mkdir ./target
+
+base_name=$1
+file=${base_name}.simf
+
+# save tests for this file
+unifdef -UTESTING -c "$file" > ./tmp/${base_name}_tests.simf
+
+# dave defines
+grep -E '^[[:space:]]*#define[[:space:]]+' "$file" > ./tmp/${base_name}_defines.simf
+
+# build without tests but with includes
+mcpp -P -I . "$file" > ./tmp/${base_name}_compiled0.simf 
+
+# concat with tests and defines
+cat ./tmp/${base_name}_defines.simf ./tmp/${base_name}_compiled0.simf ./tmp/${base_name}_tests.simf > ./tmp/${base_name}_compiled1.simf
+
+mkdir ./target/${base_name}
+
+
+# apply define for tests
+mcpp -P -I . -DTESTING ./tmp/${base_name}_compiled1.simf > ./target/${base_name}/${base_name}.simf 
+
+simply test --logging info --entrypoint ./target/${base_name}/${base_name}.simf
+
+
+rm -rf ./tmp
+
+
