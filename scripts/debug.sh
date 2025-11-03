f44fb3c8414bf7a06b8fcc83fcf8c0ba5e2f22b5
olegfomenko <oleg.fomenko2002@gmail.com>
2025-11-03 15:51:26 +0200

    fixed build script

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,11 +1,5 @@
-[ -d target ] || mkdir target
+rm -rf ./target
+mkdir ./target
 
-mcpp -P -I src main.simf -o target/global.simf
-output=$(simc --debug target/global.simf)
-
-if echo "$output" | grep -q "Result: ε"; then
-    echo "$output"
-    echo -e "\033[1;32mPassed!\033[0m"
-else
-    echo -e "\033[1;31mFailed!\033[0m"
-fi
+mcpp -P -I src main.simf -o target/main.simf
+simply build --entrypoint target/main.simf
